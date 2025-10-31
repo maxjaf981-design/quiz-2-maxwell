65029f991039c0b566a9ce19d802f4b05a19cae4
Laboratorio Estudiantes <laboratorio@ulatina.net>
2025-10-30 19:35:35 -0600

    Trabajo completo

---
+++ quiz-02/quiz-02/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace quiz_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el primer numero: ");
+            double numero1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la operacion:(suma, resta, multiplicacion, division,): ");
+            string operacion = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el segundo numero: ");
+            double numero2 = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = Calcular(numero1, numero2, operacion);
+
+            Console.WriteLine("\nEl resultado es: " + resultado);
+        }
+            static double Calcular(double numero1, double numero2, string operacion)
+            {
+                if (operacion == "suma")
+                    return numero1 + numero2;
+                else if (operacion == "resta")
+                    return numero1 - numero2;
+                else if (operacion == "multiplicacion")
+                    return numero1 * numero2;
+                else if (operacion == "division")
+                    return numero1 / numero2;
+                else
+                    return 0;
+        }
+    }
+}
